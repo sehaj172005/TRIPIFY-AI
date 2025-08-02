878f5f812ab8a8121b77373c37c0711b4d04f5d4
Sehajdeep Singh <sehajdeepp17@gmail.com>
2025-08-02 14:57:37 +0530

    initiail commit

--- Ai trip planner frontend/src/view-trip/[tripId]/index.jsx
+++ Ai trip planner frontend/src/view-trip/[tripId]/index.jsx
@@ -1,39 +1,40 @@
-import axios from 'axios';
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { toast } from 'sonner';
-import InformationSection from '../components/InformationSection.jsx'
-import Hotels from '../components/Hotels.jsx'
-import PlacesToVisit from '../components/PlacesToVisit.jsx';
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "sonner";
+import InformationSection from "../components/InformationSection.jsx";
+import Hotels from "../components/Hotels.jsx";
+import PlacesToVisit from "../components/PlacesToVisit.jsx";
 
 function Viewtrip() {
-    const {tripId} = useParams();
-    const [tripData , setTripData] = useState()
+  const { tripId } = useParams();
+  const [tripData, setTripData] = useState();
 
-   useEffect(() => {
-  const fetchTrip = async () => {
-    try {
-      const tripDetails = await axios.get(`http://localhost:3000/trip/view-trip/${tripId}`);
-      setTripData(tripDetails.data);
-      console.log("data fetched." , tripDetails.data);
-    } catch (e) {
-      toast("Can't fetch trip-details.");
-    }
-  };
+  useEffect(() => {
+    const fetchTrip = async () => {
+      try {
+        const tripDetails = await axios.get(
+          `http://localhost:3000/trip/view-trip/${tripId}`
+        );
+        setTripData(tripDetails.data);
+        console.log("data fetched.", tripDetails.data);
+      } catch (e) {
+        toast("Can't fetch trip-details.");
+      }
+    };
 
-  fetchTrip();
-
-}, [tripId]);
+    fetchTrip();
+  }, [tripId]);
 
   return (
-    <div className='p-10 md:px-20 lg:px-44 xl:px-56'>
-      <InformationSection trip = {tripData} />
-      <Hotels trip = {tripData} />
-      <PlacesToVisit trip = {tripData} />
+    <div className="p-10 md:px-20 lg:px-44 xl:px-56">
+      <InformationSection trip={tripData} />
+      <Hotels trip={tripData} />
+      <PlacesToVisit trip={tripData} />
     </div>
-  )
+  );
 }
 
-export default Viewtrip
+export default Viewtrip;
